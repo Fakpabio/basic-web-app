70def0d38f598b484c8886eb3b5cedaaeeed4cf1
Forever Akpabio <foreverakp12@gmail.com>
2023-09-15 10:18:28 -0400

    testing

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -6,6 +6,12 @@ export default function QueryProcessor(query: string): string {
       "writer in the English language and the world's pre-eminent dramatisst."
     );
   }
+  if (query.toLowerCase().includes("andrew id")) {
+    return (
+      "fakpabio"
+    );
+  }
+
 
   return "";
 }
