5599d26247bfaabff4316817a90dd9deedb275f7
Forever Akpabio <foreverakp12@gmail.com>
2023-09-19 15:50:05 -0400

    changing name

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -28,7 +28,7 @@ describe("QueryProcessor", () => {
         const query = "what is your name";
         const response: string = QueryProcessor(query);
         expect(response).toBe((
-            "forever"
+            "Akapojo"
           ));
     });
 
